6e0bfab384814206a8f73e96f2e7822039302a22
yaderick <yaderick@yaderickdeMacBook-Pro.local>
2024-10-23 10:52:34 +0800

    add

--- tests/__helpers__/registry/block.ts
+++
@@ -1,9 +0,0 @@
-import BlockBlot from '../../../src/blot/block.js';
-
-export class HeaderBlot extends BlockBlot {
-  static readonly blotName = 'header';
-  static tagName = ['h1', 'h2'];
-  static create(value?: number | string) {
-    return super.create(value) as HTMLHeadingElement;
-  }
-}
--- tests/__helpers__/registry/break.ts
+++
@@ -1,6 +0,0 @@
-import EmbedBlot from '../../../src/blot/embed.js';
-
-export class BreakBlot extends EmbedBlot {
-  static readonly blotName = 'break';
-  static tagName = 'br';
-}
